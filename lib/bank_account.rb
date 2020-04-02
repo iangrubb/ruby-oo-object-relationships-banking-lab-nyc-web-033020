fc8a6eb122a168659fe22179293f787de5192dc1
Ian Grubb <hi@iangrubb.com>
2020-04-02 13:42:39 -0400

    added git steps

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,4 +3,22 @@ class BankAccount
 # Test comment
 # V A P O R W A V E
 
+
+# git add .
+# git commit -m "helpful message"
+
+# ( saving some change you made)
+
+
+# git push
+
+# ( posting that change to github)
+
+
+# git pull
+
+# ( loading changes from github)
+
+# git clone long-ugly-link nice-name
+
 end
